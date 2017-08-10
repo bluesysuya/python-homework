45c99c8c9cd1eb3e0e31f5373a3d03853af3e5bd
bluesysuya <bluesysuya@naver.com>
2017-08-11 04:10:53 +0900

    Add 05-for-loop.py, for, range, while

---
+++ 05-for-loop.py
@@ -0,0 +1,22 @@
+# for, range
+# while
+
+# # for
+# for num in range(1, 10): # cf. [1, 2, 3][0:2], 마지막 10은 미포함
+#     print(num)
+#
+# print(" ")
+#
+# num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# for num in num_list:
+#     print(num)
+
+# while
+# while True: # 무한루프
+#     print(1)
+
+a = 1
+while a < 10:
+    print(a)
+    a = a + 1
+    
