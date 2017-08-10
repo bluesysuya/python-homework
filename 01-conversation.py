049f5de1cfec49a01020d1a67df2ad044a420f74
bluesysuya <bluesysuya@naver.com>
2017-08-11 03:00:48 +0900

    Add 01-converation.py

---
+++ 01-conversation.py
@@ -0,0 +1,12 @@
+# 화면에 데이터 출력하기
+print(1 + 2)
+
+# 사용자로부터 값 받기
+# 변수 활용
+# name = input("당신의 이름은? ")
+# print("안녕하세요, " + name)
+# print("안녕" + "하세요")
+
+num1 = input("첫번째 숫자 : ")
+num2 = input("두번째 숫자 : ")
+print(num1 + num2)
