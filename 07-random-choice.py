02897db045f13d312817f9ec5aad1f36995b048a
bluesysuya <bluesysuya@naver.com>
2017-08-11 16:46:00 +0900

    Add 07-random-choice.py, import random, random.choice

---
+++ 07-random-choice.py
@@ -0,0 +1,15 @@
+# 미니 과제 1. 랜덤 뽑기
+
+import random
+
+list_a = [1, 2, 3, 4, 5]
+
+rand_item = random.choice(list_a)
+
+print(rand_item)
+
+tuple_a = (1, 2, 3, 4, 5)
+
+rand_item = random.choice(tuple_a)
+
+print(rand_item)
