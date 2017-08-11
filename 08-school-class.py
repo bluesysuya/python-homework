496b3e2e03a2a66969f891da81307aaf0a72fbc8
bluesysuya <bluesysuya@naver.com>
2017-08-11 17:40:49 +0900

    Modified 08-school-class.py, add input

--- 08-school-class.py
+++ 08-school-class.py
@@ -6,7 +6,16 @@ class School:
         self.established = established
         self.address = address
 
-school1 = School("개발대학", "2017년 08월 07일", "이미지니어.io")
-print(school1.name)
-print(school1.established)
-print(school1.address)
+print("학교 정보를 입력하세요.")
+print(" ")
+
+name = input("학교이름 : ")
+established = input("설립년도 : ")
+address = input("주소 : ")
+
+print(" ")
+print("입력한 정보는 다음과 같습니다.")
+school1 = School(name, established, address)
+print("학교이름 : " + school1.name)
+print("설립년도 : " + school1.established)
+print("주소 : " + school1.address)
